7fb150df785374f0b6735bce106fec163f401d77
Ludgerfogg <dustyludgerfogg2@gmail.com>
2024-11-25 08:35:54 +0100

    done

---
+++ week 1.py
@@ -0,0 +1,28 @@
+# A simple calculator program in Python
+
+
+num1 = float(input("10: "))
+num2 = float(input("5: "))
+
+
+operation = input("(+, -, *, /): ")
+
+# the result
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    # Check for division by zero
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
